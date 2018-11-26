acf9159f5fcee6db3f88a20d75a1501b402cf5c9
David Cellini <david.cellini@hive.hr>
2018-11-26 21:20:59 +0000

    React setup

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>My Connect 4 game will go here</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
